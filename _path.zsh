66de6360f07ace549ae276d9f1e3d49238a5aa1a
ameya.phadke <aaphadke@gmail.com>
2018-02-17 16:02:18 -0800

    Removing unwanted files

--- _path.zsh
+++
@@ -1,2 +0,0 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/Users/ameya.phadke/Documents/workspace/tool-local-provisioner/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
